8b2c258d4ac52b11fb3b4b0d9fd3865dbc68ddb0
Dolev Farhi <farhi.dolev@gmail.com>
2021-08-26 22:54:42 -0400

    args

--- main.py
+++ main.py
@@ -12,14 +12,16 @@ from graphw00f.lib import GRAPHW00F
 
 
 def main():
-    parser = OptionParser()
+    parser = OptionParser(usage='%prog url\r\nexample: %prog -t http://www.site.org/graphql')
     parser.add_option('-r', '--noredirect', action='store_false', dest='followredirect', default=True, 
+                     
                             help='Do not follow redirections given by 3xx responses')
+    parser.add_option('-t', '--target', dest='url', help='target url with the path')
     parser.add_option('-o', '--output-file', dest='output_file', 
                             help='Output results to a file (CSV)', default=None)
     parser.add_option('-l', '--list', dest='list', action='store_true', default=False, 
                             help='List all GraphQL technologies graphw00f is able to detect')
-    parser.add_option('--version', '-V', dest='version', action='store_true', default=False, 
+    parser.add_option('--version', '-v', dest='version', action='store_true', default=False, 
                             help='Print out the current version and exit.')
     options, args = parser.parse_args()
 
@@ -33,16 +35,18 @@ def main():
                                            )
       sys.exit(0)
     
-    if not args:
-      print('Error: you must pass at least 1 url.')
-      print(f'{sys.argv[0]} https://site.com/graphql')
+    if options.version:
+      print('version:', graphw00f.helpers.VERSION)
+      sys.exit(0)
+
+    if not options.url:
+      parser.error('you must pass at least 1 url.')
       sys.exit(1)
     
-    url = args[0]
+    url = options.url
     url_path = urlparse(url).path
     url_scheme = urlparse(url).scheme
     url_netloc = urlparse(url).netloc
-    detected = None
     
     g = GRAPHW00F(follow_redirects=options.followredirect,
                   headers=conf.HEADERS, 
@@ -59,25 +63,29 @@ def main():
       sys.exit(1)
 
     if not url_path:
-      print('[*] No URL Path was provided, are you sure you want to scan for graphql without a path?...')
-      print('[*[ Continue anyway? [y/n]')
+      print('[*] No URL path was provided.')
+      print('[*[ are you sure you want to fingerprint the server without a path? [y/n]')
       choice = input().lower()
       if not graphw00f.helpers.user_confirmed(choice):
         sys.exit(1)
 
+    
+    detected = None
     print('[*] Checking if GraphQL is available at {url}...'.format(url=url))
-
+    
     if not g.check(url):
       print('[*] Continue anyway? [y/n]'.format(url=url))
+      
       choice = input().lower()
-      if graphw00f.helpers.user_confirmed(choice):
-        g.execute(url)
-      else:
+      if not graphw00f.helpers.user_confirmed(choice):
         print('Quitting.')
         sys.exit(1)
+      
+      g.execute(url)
     else:
       print('[*] Attempting to fingerprint...')
       result = g.execute(url)
+      
       if result:
         name = graphw00f.helpers.get_engines()[result]['name']
         url = graphw00f.helpers.get_engines()[result]['url']
